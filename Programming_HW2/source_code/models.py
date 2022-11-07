9f2d9effc4b8084b3f152ef84c3ee2b45885e5ca
Dingdong-LIU <dliuak@connect.ust.hk>
2022-11-07 21:46:02 +0800

    add: debug for gpu

--- Programming_HW2/source_code/models.py
+++ Programming_HW2/source_code/models.py
@@ -21,6 +21,29 @@ class MLP(nn.Module):
         x = nn.functional.relu(self.fc6(x))
         x = self.fc7(x)
         return x
+
+
+class MLP_NO_REG(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 64)
+        self.fc6 = nn.Linear(64, 32)
+        self.fc7 = nn.Linear(32, output_size)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+        x = self.fc5(x)
+        x = self.fc6(x)
+        x = self.fc7(x)
+        return x
+
 def mlp_preprocessing(x, y):
     return torch.autograd.Variable(x.view(-1, 3*32*32)), y
 
@@ -43,6 +66,33 @@ class CNN(nn.Module):
         x = x.reshape(-1, 4096)
         x = nn.functional.relu(self.fc1(x))
         x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class CNN_NO_REG(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.fc1 = nn.Linear(4096, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, output_size)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = x.reshape(-1, 4096)
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc3(x)
         return x
 
